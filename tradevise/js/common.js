bdc5bae2ac255bbc2593b7b455f4cf9867437a0a
Pavlo Hupalo <pashkogup@gmail.com>
2017-08-22 14:31:42 +0300

    add

---
+++ tradevise/js/common.js
@@ -0,0 +1,76 @@
+$(function() {
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(this).height()) {
+			$('.side-menu').addClass('active');
+		} else {
+			$('.side-menu').removeClass('active');
+		}
+	});
+
+	//$('.top').click(function () {
+	//	$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	//});
+
+	$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
+
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
+
+ $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
+  var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+      $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+      return false; // выключаем стандартное действие
+    });
+
+$(".loginfr").magnificPopup();
+$(".registrr").magnificPopup();
+
+$(window).on('load', function () {
+  $('.preloader').delay(1000).fadeOut('slow');
+});
+
+});
+
+
